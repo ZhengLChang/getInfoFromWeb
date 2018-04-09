0855996d35a5e8a06bfb229bb72c3239f45a8cf5
zhenghuijie <17688721902@163.com>
2018-04-09 20:34:34 +0800

    add iconv

--- util.c
+++ util.c
@@ -1,4 +1,5 @@
 #include "util.h"
+#include "iconv.h"
 struct error_data error_array[] =
 {
 	{NO_ERROR, "no error"},
@@ -542,4 +543,46 @@ ERR:
 	}
 	return ;
 }
+ 
+static int charset_convert(const char *from_charset, const char *to_charset,
+                          char *in_buf, size_t in_left, char *out_buf, size_t out_left)
+{
+	iconv_t icd = (iconv_t)-1;
+	size_t sRet = -1;
+	char *pIn = in_buf;
+	char *pOut = out_buf;
+    	size_t outLen = out_left;
+    	if (NULL == from_charset || NULL == to_charset || NULL == in_buf || 0 >= in_left || NULL == out_buf || 0 >= out_left)
+	{
+		return -1;
+	}
 
+	icd = iconv_open(to_charset, from_charset);
+	if ((iconv_t)-1 == icd)
+	{
+		fprintf(stderr, "%s %d, iconv_open %s\n", __func__, __LINE__, strerror(errno));
+		return -1;
+	}
+
+	sRet = iconv(icd, &pIn, &in_left, &pOut, &out_left);
+	if ((size_t)-1 == sRet)
+	{
+		fprintf(stderr, "%s %d, iconv_open %s\n", __func__, __LINE__, strerror(errno));
+		iconv_close(icd);
+    		return -1;
+       	}
+	out_buf[outLen - out_left] = 0;
+	iconv_close(icd);
+	return (int)(outLen - out_left);
+}
+ 
+int charset_convert_UTF8_TO_GB2312(char *in_buf, size_t in_left, char *out_buf, size_t out_left)
+{
+	return charset_convert("UTF-8", "GB2312", in_buf, in_left, out_buf, out_left);
+}
+ 
+int charset_convert_GB2312_TO_UTF8(char *in_buf, size_t in_left, char *out_buf, size_t out_left)
+{
+	return charset_convert("GB2312", "UTF-8", in_buf, in_left, out_buf, out_left);
+}
+ 
--- util.h
+++ util.h
@@ -167,5 +167,7 @@ enum {
 };
 
 extern struct error_data error_array[];
+int charset_convert_UTF8_TO_GB2312(char *in_buf, size_t in_left, char *out_buf, size_t out_left);
+int charset_convert_GB2312_TO_UTF8(char *in_buf, size_t in_left, char *out_buf, size_t out_left);
 
 #endif
